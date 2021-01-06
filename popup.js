cc88b09f036aee546fdba322f48650d2de110dfe
songxing10000 <songxing10000@live.cn>
2021-01-06 13:39:47 +0800

    up

--- popup.js
+++ popup.js
@@ -4,52 +4,7 @@ var url;
 // 监听来消息 getSource
 chrome.runtime.onMessage.addListener(function (request, sender) {
   if (request.action == "getSource") {
-    if (url.indexOf('http://tool.chinaz.com/dns?') >= 0) {
-      message.innerText = '\n' + request.source;
-    } else if (url.indexOf('/merge_requests/new') >= 0) {
-
-    } else if (url.indexOf('translate.google.cn') >= 0) {
       message.innerText = request.source;
-    } else if (url.indexOf('gateway-manager') >= 0) {
-      
-      message.innerText = request.source;
-
-  } else if (url.indexOf('weex.json') >= 0) {
-      if (request.source.indexOf('{"page') >= 0) {
-        message.innerText = 'decrypted'
-        return;
-      }
-      // popup.js 回显示 网页里 信息。
-      let key = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDkAh06uqqrA8qIsyd98'
-      let iv = 'kWeUizGkPbW2AKR7bXK3W71l7U7VN/+1ohd0kuFLbnjTCbp8nTJUQIDAQAB'
-      var keyHex = CryptoJS.enc.Utf8.parse(key);
-      var ivHex = CryptoJS.enc.Utf8.parse(iv);
-      // Triple DES 解密
-      var decrypted = CryptoJS.TripleDES.decrypt(request.source, keyHex, {
-        iv: ivHex,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-      });
-
-      // 转换为 utf8 字符串
-      decrypted = CryptoJS.enc.Utf8.stringify(decrypted);
-      
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(tabs[0].id, {
-                code: 'document.childNodes[0].innerText = ' + JSON.stringify(decrypted.toString())
-            }, function () {
-              if (chrome.runtime.lastError) {
-                message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-              } else {
-                message.innerText = 'decrypted'
-              }
-            })
-        })
-      // message.innerText = '/// ' + title + url + '\n' + '#define ' + request.source;
-    } else  {
-      message.innerText = request.source;
-    }
-
   }
 });
 
